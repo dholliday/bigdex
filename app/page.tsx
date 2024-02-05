22046a67d47df48ace1b0398749fb7fdf5ae1ca4
Dave <holliday.david@gmail.com>
2024-02-05 22:53:10 +0000

    updated to be more dexy

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,20 @@
 "use client";
 
 import { Heading, Box, Text, Center } from "@chakra-ui/react";
-import { useWallet } from "@solana/wallet-adapter-react";
-import MintForm from "@/components/MintForm";
 
 export default function Home() {
-  const { connected } = useWallet();
-
   return (
-    <main>
+    <Box as="main">
       <Box as="section" bg="#2496b1" color="white" pb="4em" pt="1em">
         <Center>
-          <Heading fontSize="6em">NFT Minter</Heading>
+          <Heading fontSize="6em">$BIGDEX</Heading>
         </Center>
         <Center>
           <Text pt="1em" fontSize="2em">
-            Oh nice...another minter?
+            How big is your dex? Eat stake. Grow $BIGDEX
           </Text>
         </Center>
       </Box>
-      <Box pt="2em">{connected ? <MintForm /> : ""}</Box>
-    </main>
+    </Box>
   );
 }
